8e8306db1efb8e6ead9dbcf72fc5787fb79ad34d
Suave101 <57422635+Suave101@users.noreply.github.com>
2019-11-12 19:09:41 -0600

    Animation.js
    v1.1

--- Animation.js
+++ Animation.js
@@ -1,13 +1,16 @@
 var x = 50;
 stroke("red");
+fill("red");
 function draw() {
   background("green");
   shape(200, x, 50, 200, 200, 200);
   if (x<400) {
     x = x+1;
   } else {
-    while (x>0) {
+    while (x!=0) {
       x = x-1;
+      shape(200, x, 50, 200, 200, 200);
     }
   }
 }
+
